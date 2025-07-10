cfd548651132a78391de281ecdf5a088bd4a9944
saveliev <saveliev@tecforce.ru>
2025-07-10 09:25:35 +0400

    bridge, fix

--- grand/src/ru/redactor/patterns/grand/structural/bridge/StreamingSensorImpl.java
+++ grand/src/ru/redactor/patterns/grand/structural/bridge/StreamingSensorImpl.java
@@ -2,7 +2,7 @@ package ru.redactor.patterns.grand.structural.bridge;
 
 public interface StreamingSensorImpl extends SimpleSensorImpl {
 
-    public void setSamplingFreguency (int freq) throws SensorException ;
+    public void setSamplingFrequency (int freq) throws SensorException ;
 
     public void setStreamingSensorListener (StreamingSensorListener listener) ;
 
